4400d0b374a61d4fffcc79498f8c0675f33bd5ed
Animesh Pathak <animeshpathak2317@gmail.com>
2026-04-17 12:29:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173','https://samadhanprj.netlify.app'], credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
